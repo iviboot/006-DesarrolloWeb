0823f345d1e775c05c10eed7d55b02139fb3452e
Ivan Almaraz <trampitas2017@gmail.com>
2024-03-15 20:17:28 -0600

    segundo repo

---
+++ app.js
@@ -0,0 +1,113 @@
+document.getElementById("content").style.color = "azure";
+
+document.getElementById("titulo").innerHTML = "Hello World!";
+
+document.getElementById("content").style.backgroundColor = "orange";
+
+document.getElementById("parrafo").innerHTML = "Atomo editor de codigo";
+
+document.getElementById("parrafo").style.fontWeight = "bold";
+
+document.getElementById("parrafo").style.color = "red";
+
+document.images[0].style.border = "3px solid cadetblue "; 
+
+//document.images[0].style.borderRadius = "50%";
+
+document.images[0].style.width = "180px"
+
+document.images[0].style.right = "380px"
+
+document.getElementById("content2").style.display = "flex";
+
+document.getElementById("parrafo2").innerHTML = "GitHub  anuncio el fin de la aplicación, cuyo término se dio el  15 de diciembre de 2022; desarrollado en 2011 usando Electron y mantenido por la propia GitHub, un editor que está pensado tanto para cubrir las necesidades de los usuarios que empiezan a dar los primeros pasos dentro de la programación como para aquellos más avanzados que crean programas y webs, cuenta con soporte para una gran cantidad de lenguajes de programación, como C++. PHP, HTML y JavaScript..";
+
+document.getElementById("parrafo3").innerHTML = "A pesar de haber tenido una gran acogida desde el primer día, finalmente no ha podido con la presión de la competencia, como Visual Studio Code, ni con el auge que está teniendo la nube en todo tipo de tareas, especialmente en aquellas relacionadas con la programación. Y, por ello, finalmente GitHub ha decidido descontinuar el editor.";
+
+document.getElementById("parrafo4").innerHTML = "La tecnología Electron empezó a ganar popularidad gracias al lanzamiento de este editor de código. Sin embargo, a pesar de su fin de vida, esta va a seguir funcionando en todo tipo de proyectos, como Microsoft Visual Studio Code, Slack y el propio GitHub Desktop, por lo que el cierre de Atom no significa el final de Electron.";
+
+document.getElementById("parrafo5").innerHTML = "uno de los principales contribuyentes de Atom, Max Brunsfeld, ya estiene listo un sucesor espiritual (escrito en Russt) llamado Zed, que se lanzo en versión alfa .";
+
+document.getElementById("content2").style.alignItems = "center";
+
+document.getElementById("parrafo6").innerHTML = "Los editores de texto  son programas que permiten crear y modificar archivos digitales compuestos únicamente por texto sin formato, conocidos comúnmente como archivos de texto o texto plano. El programa lee el archivo e interpreta los bytes leídos según el código de caracteres que usa el editor. Hoy en día es comúnmente de 7- ó 8-bits en ASCII o UTF-8, rara vez EBCDIC.";
+
+document.getElementById("parrafo2").style.fontWeight = "bold";
+
+document.getElementById("parrafo3").style.fontWeight = "bold";
+
+document.getElementById("parrafo4").style.fontWeight = "bold";
+
+document.getElementById("parrafo5").style.fontWeight = "bold";
+
+document.getElementById("parrafo6").style.fontWeight = "bold";
+
+document.getElementById("parrafo7").innerHTML = "Por ejemplo, un editor ASCII de 8 bits que lee el número binario 0110 0001 (decimal 97 ó hexadecimal 61) en el archivo lo representará en la pantalla por la figura a, que el usuario reconoce como la letra (a) y ofrecerá al usuario las funciones necesarias para cambiar el número binario en el archivo. Los editores de texto son incluidos en el sistema operativo o en algún paquete de software instalado , scripts o el código fuente de algún programa";
+
+document.getElementById("parrafo7").style.fontWeight = "bold";
+
+document.getElementById("content3").style.backgroundColor = "lightskyblue";
+
+document.getElementById("content3").style.color = "azure";
+
+document.getElementById("parrafo8").style.fontWeight = "bold";
+
+document.getElementById("parrafo8").innerHTML = "referencias";
+
+document.getElementById("content4").style.backgroundColor = "darkcyan";
+
+document.getElementById("content4").style.color = "azure";
+
+document.getElementById("img1").style.color = "azure";
+
+document.getElementById("img2").style.color = "azure";
+
+document.getElementById("img3").style.color = "azure";
+
+document.getElementById("img4").style.color = "azure";
+
+document.getElementById("img5").style.color = "azure";
+
+document.getElementById("img1").style.fontWeight = "bold";
+
+
+document.getElementById("img2").style.fontWeight = "bold";
+
+document.getElementById("img3").style.fontWeight = "bold";
+
+document.getElementById("img4").style.fontWeight = "bold";
+
+document.getElementById("img5").style.fontWeight = "bold";
+
+document.getElementById("content4").style.border = "5px solid lightskyblue";
+
+
+document.getElementById("content").style.border = "5px solid tomato";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
